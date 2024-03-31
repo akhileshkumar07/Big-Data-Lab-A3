6f7320462f0831f40b6f1641d062dfef305f5219
akhileshkumar07 <akhilesh.kumar070197@gmail.com>
2024-03-31 15:20:29 +0530

    Script to download CSV files

--- download.py
+++ download.py
@@ -2,11 +2,16 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import random
+import yaml
 
 BASE_URL = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/'
-YEAR = '2021'
 OUTPUT_PATH = '/home/akhilesh/big-data-lab/'
 NUM_FILES = 1
+
+# Load YEAR variable from params.yaml
+with open('params.yaml', 'r') as yaml_file:
+    config = yaml.safe_load(yaml_file)
+    YEAR = config['YEAR']
 
 # Fetch the webpage of the datasets
 page_response = requests.get(BASE_URL + YEAR + '/')
@@ -15,25 +20,23 @@ if page_response.status_code == 200:
         page_file.write(page_response.content)
 
 # Extract csv file links
-def extract_csv_links(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    csv_links = [link.get('href') for link in soup.find_all('a') if link.get('href').endswith('.csv')]
-    return csv_links
-
 with open(os.path.join(OUTPUT_PATH, 'page_fetch.html'), 'r', encoding='utf-8') as file:
     html_content = file.read()
-    csv_links = extract_csv_links(html_content)
+    soup = BeautifulSoup(html_content, 'html.parser')
+    csv_links = [link.get('href') for link in soup.find_all('a') if link.get('href').endswith('.csv')]
 
 # Select random data files from the available list of csv files
 selected_files = random.sample(csv_links, NUM_FILES)
 
 # Fetch individual csv files
+print("Downloading CSV Files...")
 for filename in selected_files:
     file_url = BASE_URL + YEAR + '/' + filename
     response = requests.get(file_url)
     if response.status_code == 200:
         with open(os.path.join(OUTPUT_PATH, filename), 'wb') as file:
             file.write(response.content)
+print("Download Complete.")
 
 # Delete the page_fetch.html file
 os.remove(os.path.join(OUTPUT_PATH, 'page_fetch.html'))
